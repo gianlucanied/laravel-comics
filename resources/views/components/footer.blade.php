c1cd39cdf0e9c9a4974147afbd48f3c1c0f10988
gianlucanied <gianluca.nieddu96@gmail.com>
2024-02-09 15:45:22 +0100

    Add: header e footer + img

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,60 @@
-<footer>
-    <h1>Hello from footer</h1>
-</footer>
+<div class="footer">
+    <div class="footer-top d-flex">
+        <div class="link d-flex justify-content-center flex-wrap g-2">
+            <ul>
+                <li class="first">DC COMICS</li>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+            </ul>
+
+            <ul>
+                <li class="first">DC</li>
+                <li>Terms Of Use</li>
+                <li>Privacy policy (New)</li>
+                <li>Ad Choices</li>
+                <li>Advertising</li>
+                <li>Jobs</li>
+                <li>Subscriptions</li>
+                <li>Talent Workshops</li>
+                <li>CPSC Certificates</li>
+                <li>Ratings</li>
+                <li>Shop Help</li>
+                <li>Contact Us</li>
+            </ul>
+
+            <ul>
+                <li class="first">SITES</li>
+                <li>DC</li>
+                <li>MAD Magazine</li>
+                <li>DC Kids</li>
+                <li>DC Universe</li>
+                <li>DC Power Vise</li>
+            </ul>
+            <ul class="shop">
+                <li class="first">SHOP</li>
+                <li>Shop DC</li>
+                <li>Shop DC Collectibles</li>
+            </ul>
+        </div>
+        <div class="logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo-bg.png')}}" alt="Dc Logo" />
+
+        </div>
+    </div>
+    <div class="footer-bottom d-flex justify-content-around align-items-center">
+        <button> <span>SIGN-UP NOW!</span></button>
+        <div class="social d-flex aling-center-center">
+            <span class="follow">FOLLOW US</span>
+            <img src="{{ Vite::asset('resources/img/footer-pinterest.png')}}"/>
+            <img src="{{ Vite::asset('resources/img/footer-twitter.png')}}" />
+            <img src="{{ Vite::asset('resources/img/footer-youtube.png')}}" />
+            <img src="{{ Vite::asset('resources/img/footer-pinterest.png')}}"/>
+            <img src="{{ Vite::asset('resources/img/footer-periscope.png')}}" />
+        </div>
+    </div>
+</div>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,19 @@
-<header>
-    <h1>Hello from Header</h1>
-</header>
+<div class="nav d-flex justify-content-around align-items-center">
+    <div class="logo">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="Dc Logo" />
+    </div>
+    <div class="voices">
+        <a href="#"> CHARACTERS </a>
+        <a href="#"> COMICS </a>
+        <a href="#"> MOVIES </a>
+        <a href="#"> TV </a>
+        <a href="#"> GAMES </a>
+        <a href="#"> COLLECTIBLES </a>
+        <a href="#"> VIDEOS </a>
+        <a href="#"> FANS </a>
+        <a href="/news"> NEWS </a>
+        <a href="/shop"> SHOP </a>
+    </div>
+
+    <li><input class="form-control" type="text" placeholder="Search"></li>
+</div>
